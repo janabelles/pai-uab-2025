4814e53a9cb928e287fc53b77ef57c26473986d7
janabelles <janabp2006@gmail.com>
2025-10-10 16:50:46 +0200

    orde

---
+++ JS/Exercici 13_condicionals.js
@@ -0,0 +1,18 @@
+//13.1.- Escriu un programa que donat un número llegit amb prompt escrigui el missatge “el número llegit és negatiu” si el número és més petit que zero. //
+let numero = parseInt(promt("Introdueix un número:"));
+if (numero < 0) {
+    console.log("El número llegit és negatiu));")
+}
+
+//13.2.- Escriu un programa que donat un número llegit amb prompt, escrigui el missatge “el número llegit és parell” si el número es parell. //
+let numero = parseInt(promt("Introdueix un número:"));
+if (numero % 2 === 0) {
+    console.log("El número llegit és parell");
+}
+
+//13.3.- Escriu un programa que, donat un número llegit amb prompt, escrigui “el número llegit és parell” si el número es parell, o “el número llegit és senar”, si és senar. Fes servir un if per cada cas.//
+let numero = parseInt(promt("Introdueix un número:"));
+if (numero % 2 === 0) {
+    console.log("El número llegit és parell");
+}
+
